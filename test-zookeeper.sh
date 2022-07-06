63766aea0886848950cda4716b1023e6bdfd2ff0
Chris Richardson <chris@chrisrichardson.net>
2022-07-06 11:46:41 -0700

    Improved ZK tests

--- test-zookeeper.sh
+++ test-zookeeper.sh
@@ -1,13 +1,38 @@
 #! /bin/bash -e
 
-NODE=/foo$(date +%Y%m%d%H%M%S)
+ID=-$(date +%Y%m%d%H%M%S)
+RN=zookeeper$ID
+POD=$RN-0
+NODE=/foo$ID
+POD_EXEC="kubectl exec $POD -- bash -c "
+POD_EXEC_I="kubectl exec -i $POD -- bash -c "
 
-kubectl exec zookeeper-0 -- bash -c "cd /usr/local/apache-zookeeper-3.5.6-bin ; echo create $NODE | bin/zkCli.sh -server zookeeper:2181"
-kubectl exec zookeeper-0 -- bash -c "cd /usr/local/apache-zookeeper-3.5.6-bin ; echo set $NODE bar | bin/zkCli.sh -server zookeeper:2181"
-kubectl exec zookeeper-0 -- bash -c "cd /usr/local/apache-zookeeper-3.5.6-bin ; echo get $NODE | bin/zkCli.sh -server zookeeper:2181"
+echo installing
 
-# kubectl exec zookeeper-0 -- bash -c "cd /usr/local/apache-zookeeper-3.5.6-bin ; echo get /foo | bin/zkCli.sh -server zookeeper:2181"
+helm install --wait $RN charts/zookeeper
+
+echo testing
+
+$POD_EXEC_I "bin/zkCli.sh -server zookeeper:2181" <<END
+create $NODE
+set $NODE bar
+get $NODE 
+END
+
+echo deleting
+
+kubectl delete po $POD
+
+kubectl wait --for=condition=ready pod --timeout=90s $POD
+
+echo testing persistence
+
+$POD_EXEC "echo get $NODE | bin/zkCli.sh -server zookeeper:2181"
+
+# kubectl exec zookeeper-0 -- bash -c "echo get /foo | bin/zkCli.sh -server zookeeper:2181"
 
 echo
 echo
 echo SUCCESS
+
+helm uninstall $RN
