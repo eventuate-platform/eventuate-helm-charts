431b89ed3b3c6ae3558eb6cd400540618b74d39d
Chris Richardson <chris@chrisrichardson.net>
2022-10-17 13:53:01 -0700

    Fixed generated config

--- .circleci/generate-config.sh
+++ .circleci/generate-config.sh
@@ -10,19 +10,20 @@ orbs:
           script:
             description: the script to execute
             type: string
-    machine:
-      image: ubuntu-2004:202101-01
-    steps:
-      - add_ssh_keys:
-          fingerprints:
-            - "0a:2e:d8:65:0d:6f:70:5b:1e:a1:45:70:5f:8a:72:e6"
-      - checkout
-      - run: sudo ./install-k8s-tools.sh
-      - run: kind create cluster
-      - run: <<parameters.script>>     
+        machine:
+          image: ubuntu-2004:202101-01
+        steps:
+          - add_ssh_keys:
+              fingerprints:
+                - "0a:2e:d8:65:0d:6f:70:5b:1e:a1:45:70:5f:8a:72:e6"
+          - checkout
+          - run: sudo ./install-k8s-tools.sh
+          - run: kind create cluster
+          - run: <<parameters.script>>     
 workflows:
   version: 2.1
   build-test-and-deploy:
+    jobs:
 END
 
 for build_script in test-*.sh ; do
